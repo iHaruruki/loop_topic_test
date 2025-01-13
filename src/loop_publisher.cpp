3a508523164df87ba0b795070eaa9c5713a1f7bf
Haruki Isono <haruki.isono861@gmail.com>
2025-01-14 01:31:26 +0900

    fix: エラー解消update executable names and improve formatting in loop publisher and subscriber

--- src/loop_publisher.cpp
+++ src/loop_publisher.cpp
@@ -4,21 +4,21 @@
 
 int main(int argc, char **argv)
 {
-	rclcpp::init( argc, argv ); // ノードを初期化
-	auto node = rclcpp::Node::make_shared( "my_publisher" ); // ノードの 生成
+	rclcpp::init(argc,argv); // ノードを初期化
+	auto node = rclcpp::Node::make_shared("my_publisher"); // ノードの 生成
 	// String 型のメッセージを扱う greeting トピックを生成
-	auto pub_greeting = node->create_publisher<std_msgs::msg::String>( "greeting", 1 );
-	
-	rclcpp::WallRate loop( 1 ); // ループ周期を 1Hz に設定
+	auto pub_greeting = node->create_publisher<std_msgs::msg::String>("greeting",1);
+
+	rclcpp::WallRate loop(1); // ループ周期を 1Hz に設定
 	int count = 0;
-	
-	while ( rclcpp::ok( ) ) {
-		auto msg = std_msgs::msg::String( ); // String 型のメッセージを生成
-		msg.data = "Hello world " + std::to_string( count++ );
-		pub_greeting->publish( msg ); // メッセージを配信
-		RCLCPP_INFO( node->get_logger( ), "%s", msg.data.c_str( ) );
-		loop.sleep( ); // 周期が 1Hz になるように時間調整
+
+	while (rclcpp::ok()){
+		auto msg = std_msgs::msg::String(); // String 型のメッセージを生成
+		msg.data = "Hello world " + std::to_string(count++);
+		pub_greeting->publish(msg); // メッセージを配信
+		RCLCPP_INFO(node->get_logger(),"%s", msg.data.c_str());
+		loop.sleep(); // 周期が 1Hz になるように時間調整
 	}
-	rclcpp::shutdown( ); // 通信を切断
+	rclcpp::shutdown(); // 通信を切断
 	return 0;
 }
--- src/loop_subscriber.cpp
+++ src/loop_subscriber.cpp
@@ -3,23 +3,26 @@
 
 
 // String 型のメッセージを受け取るコールバック関数を定義
-void callback( const std_msgs::msg::String::SharedPtr msg )
+void callback(const std_msgs::msg::String::SharedPtr msg)
 {
-	RCLCPP_INFO( rclcpp::get_logger( ), "%s", msg->data.c_str( ) );
+	//RCLCPP_INFO(rclcpp::get_logger(), "%s", msg->data.c_str());
+	RCLCPP_INFO(rclcpp::get_logger("loop_subscriber"), "%s", msg->data.c_str());
+
 }
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-	rclcpp::init( argc, argv );
-	auto node = rclcpp::Node::make_shared( "my_subscriber" );
+	rclcpp::init(argc, argv);
+	auto node = rclcpp::Node::make_shared("my_subscriber");
 	// String 型のメッセージを greeting トピック経由で受け取ったら callback 関数を実行する
-	auto sub_greeting = node->create_subscription<std_msgs::msg::String>( "greeting", 1, callback );
+	auto sub_greeting = node->create_subscription<std_msgs::msg::String>("greeting", 1, callback);
 	// callback の永続待ち． main 関数はブロックされる． Ctrl+C などで終了させると先に進む．
-	while ( rclcpp::ok( ) )
+	while (rclcpp::ok())
 	{
-		rclcpp::spin_some( node );
+		rclcpp::spin_some(node);
 	}
-	g_node = nullptr;
-	rclcpp::shutdown( );
+	//g_node = nullptr;
+	node = nullptr;
+	rclcpp::shutdown();
 	return 0;
 }
